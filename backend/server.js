605d54c18faa62739e5f3f815970b36780757c53
JozefVargas <futureisnear2050@gmail.com>
2024-07-02 16:44:23 -0400

    Removed helmet for initial testing

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,13 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const path = require('path');
-const helmet = require('helmet');  // Helmet used to handle HTTP headers.
+// const helmet = require('helmet');  // Commenting out helmet for now
+
 const app = express();
 const port = process.env.PORT || 4080;
 
 app.use(bodyParser.json());
 app.use(cors());
-
-//Content Security Policy
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "https:"],
-        connectSrc: ["'self'"],
-        fontSrc: ["'self'", "https:", "data:"],
-        objectSrc: ["'none'"],
-        mediaSrc: ["'self'"],
-        frameSrc: ["'self'"]
-    }
-}));
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'frontend/build')));
